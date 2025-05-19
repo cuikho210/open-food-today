6d93606ecf23138b3a0a6c0a89fd60b03c609835
cuikho210 <tonykms210@gmail.com>
2025-05-19 15:57:02 +0700

    make recipe author nullable

--- server/migrate/migrations/001_recipes.sql
+++ server/migrate/migrations/001_recipes.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS public.recipes (
     title VARCHAR(255) NOT NULL,
     link VARCHAR(512),
     description TEXT,
-    author BIGINT NOT NULL,
+    author BIGINT,
     image_url VARCHAR(512),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
