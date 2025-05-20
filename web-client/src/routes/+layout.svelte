07fb7b5ed51a304cb40f2183a7ba042792fe37df
cuikho210 <tonykms210@gmail.com>
2025-05-20 19:52:26 +0700

    init web-client

---
+++ web-client/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '@celar-ui/svelte/src/styles/colors.scss';
+	import '@celar-ui/svelte/src/styles/spacing.scss';
+	import '@celar-ui/svelte/src/styles/misc.scss';
+	import '$styles/index.scss';
+	import '$styles/utils.scss';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ web-client/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Container } from '@celar-ui/svelte';
+</script>
+
+<Container sm>
+	<h1>Ahihi</h1>
+</Container>
