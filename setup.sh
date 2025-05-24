5d4741bd11f413b217510883cb7b174c65ed1e18
cuikho210 <tonykms210@gmail.com>
2025-05-24 16:29:37 +0700

    remove flutter aliases from setup script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,4 @@ cd "$(dirname "$0")"
 export $(cat ".env" | xargs)
 alias cm=$PWD/commit.sh
 
-PID_FILE="/tmp/flutter.pid"
-alias flutter-run="cd $PWD/flutter-client && flutter run --pid-file $PID_FILE"
-alias flutter-watch="find $PWD/flutter-client/lib/ -name '*.dart' | entr -p kill -USR1 \$(cat /tmp/flutter.pid)"
-alias f-r="flutter-run"
-alias f-w="flutter-watch"
-
 cd -
