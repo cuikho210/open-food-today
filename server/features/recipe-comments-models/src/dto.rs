c406f1970955313ae20244134a7a63bd2c898fc9
cuikho210 <tonykms210@gmail.com>
2025-05-29 23:27:34 +0700

    generate TypeScript types for comment DTOs

--- server/features/recipe-comments-models/src/dto.rs
+++ server/features/recipe-comments-models/src/dto.rs
@@ -1,12 +1,15 @@
 use serde::{Deserialize, Serialize};
+use ts_rs::TS;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, TS)]
+#[ts(export, export_to = "recipe_comments.d.ts")]
 pub struct CreateCommentPayload {
     pub reply_to: Option<i64>,
     pub content: String,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, TS)]
+#[ts(export, export_to = "recipe_comments.d.ts")]
 pub struct PaginationData {
     pub limit: Option<i64>,
     pub last_id: Option<i64>,
